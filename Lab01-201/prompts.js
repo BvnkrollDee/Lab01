acd156b8d4fd9018fd9a0276e81f3dc8f7051650
Daron Faulkner Jr <dj200099@gmail.com>
2023-03-02 11:40:11 -0600

    updated

---
+++ Lab01-201/prompts.js
@@ -0,0 +1,18 @@
+function Question() {
+
+let p1 = prompt("When is your birthday?");
+alert("Awesome ");
+
+let p2 = prompt("Where are you from");
+alert("I think I know someone from there");
+
+let p3 = prompt("What is your favorite food?");
+alert("I heard that was pretty good");
+
+let p4 = prompt("What is your favorite hobby?");
+alert("That's interesting!!")
+// console.log("I'm running"); 
+
+}
+
+Question()
